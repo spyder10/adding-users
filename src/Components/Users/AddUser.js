7ca28cc1e7e5ff34b4ea41a4762aa1c01a491032
spyder10 <hi.vibhor10@gmail.com>
2021-10-19 14:10:01 +0530

    Added refs for reading input...dropping input states

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,34 +1,40 @@
 import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
-import { useState, Fragment } from "react";
+import { useState, Fragment ,useRef } from "react";
 import Modal from '../UI/ErrorModal';
 
 function AddUser(props) {
-  const [enteredText, setEnteredText] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  // const [enteredText, setEnteredText] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const[error,setError] = useState(false);
 
-  const textChangeHandler = (event) => {
-    setEnteredText(event.target.value);
-  };
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const textChangeHandler = (event) => {
+  //   setEnteredText(event.target.value);
+  // };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    if (enteredText.trim().length === 0 || enteredAge.trim().length < 1) {
+    if (nameInputRef.current.value.length === 0 || ageInputRef.current.value.trim().length < 1) {
         setError(true);
       return;
     }
     const detailsNewUser = {
-      username: enteredText,
-      age: enteredAge,
+      username: nameInputRef.current.value,
+      age: ageInputRef.current.value,
       key: Math.random().toString(),
     };
     props.onAddNewUser(detailsNewUser);
-    setEnteredAge("");
-    setEnteredText("");
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    // setEnteredAge("");
+    // setEnteredText("");
   };
 const clickHandler = () =>{
     setError(false);
@@ -45,14 +51,16 @@ const clickHandler = () =>{
           <label>Username</label>
           <input
             type="text"
-            value={enteredText}
-            onChange={textChangeHandler}
+            // value={nameInputRef.current.value}    // Don't have to feed values here (2-way) as values are directly changed in DOM using ref in submitHandler.
+            // onChange={textChangeHandler}
+            ref={nameInputRef}
           ></input>
           <label>Age (In Years)</label>
           <input
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // value={ageInputRef.current.value}
+            // onChange={ageChangeHandler}
+            ref={ageInputRef}
           ></input>
           <Button type="submit">Add User</Button>
         </form>
